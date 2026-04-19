c7f8e1dc9194321e3faedb9888e46721ed7c4865
Tom <tom.cohen.06pro@gmail.com>
2026-04-19 17:37:21 +0200

    Mise à jour automatique

--- htdocs/pushToGithub.py
+++ htdocs/pushToGithub.py
@@ -3,23 +3,17 @@ import os
 
 ROOT = r"C:\Users\Tom_Le_BG\Desktop\Projets_Sites\cohen-tom.github.io\htdocs"
 
-def run(cmd):
-    result = subprocess.run(cmd, shell=True)
-    if result.returncode != 0:
-        print(f"❌ Erreur : {cmd}")
-        exit()
-
 def main(commit_message=None):
     print("📦 Ajout des fichiers...")
     # Plus besoin de syntaxe complexe ou d'exclusion manuelle !
     # Git lira automatiquement ton .gitignore et n'ajoutera que ce qui est autorisé.
-    run('git add .') 
+    subprocess.run('git add .') 
 
     print("📝 Commit...")
     
     if not commit_message:
         commit_message = "Mise à jour automatique"  # Message de commit par défaut
-    run(f'git commit -m "{commit_message}"')
+    subprocess.run(f'git commit -m "{commit_message}"')
 
 if __name__ == "__main__":
     main()
