bae88feae2be2213354900e3c39c21d27e8f1e9a
Tom <tom.cohen.06pro@gmail.com>
2026-05-29 21:03:25 +0200

    Mise à jour automatique

---
+++ htdocs/Cours-C++/exemple/prog.cc
@@ -0,0 +1,24 @@
+// Television.h
+
+/*
+class Television {
+
+private:
+    //Attributs
+    int chaine;
+    bool estAllumee;
+    string marque;
+    int volume;
+
+public:
+    //Méthodes
+    void Allumer();
+    void Eteindre();
+    void SetVolume(int vol);
+    void ChangerChaine(int c);
+    int GetVolume();
+
+    //Construteur
+    Television(string marque);
+};
+*/
